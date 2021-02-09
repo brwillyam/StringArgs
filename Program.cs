caabb5c43eac8a840e25503e5b74a0bb6ed6d788
Willyam D <br.willyam@hotmail.com>
2021-02-09 15:02:09 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string nome = "";
+             
+
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Por favor insira um nome");
+            }
+            else
+            {
+                nome = args[0];
+                string nomeinvertido = new string(nome.Reverse().ToArray());
+
+                Console.WriteLine(nomeinvertido);
+            }
+        }
+    }
+}
